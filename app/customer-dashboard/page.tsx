c6447713120e40656cb8d3d85b816973087c462c
matellordson <snr.mn.lordson@gmail.com>
2024-09-08 18:19:52 +0100

    cmt

--- app/customer-dashboard/page.tsx
+++ app/customer-dashboard/page.tsx
@@ -11,30 +11,32 @@ export default function CustomerDashboard() {
           <div className="mb-4 grid grid-cols-2 gap-4">
             <div>
               <p className="text-sm font-semibold">Package ID:</p>
-              <p className="text-sm">SwIFT J75H84G</p>
+              <p className="text-sm text-muted-foreground">SwIFT J75H84G</p>
             </div>
             <div>
               <p className="text-sm font-semibold">Estimated Delivery:</p>
-              <p className="text-sm">2023-06-15</p>
+              <p className="text-sm text-muted-foreground">2023-06-15</p>
             </div>
             <div>
               <p className="text-sm font-semibold">Origin:</p>
-              <p className="text-sm">San Francisco, CA</p>
+              <p className="text-sm text-muted-foreground">San Francisco, CA</p>
             </div>
             <div>
               <p className="text-sm font-semibold">Destination:</p>
-              <p className="text-sm">New York, NY</p>
+              <p className="text-sm text-muted-foreground">New York, NY</p>
             </div>
             <div>
               <p className="text-sm font-semibold">Status:</p>
-              <div className="flex items-center justify-start gap-1 text-xs">
+              <div className="flex items-center justify-start gap-1 text-xs text-muted-foreground">
                 <div className="h-2 w-2 rounded-full bg-orange-300"></div>
                 Processing
               </div>
             </div>
             <div>
-              <p className="text-sm font-semibold">Package Type:</p>
-              <p className="text-sm">Furniture</p>
+              <p className="text-sm font-semibold text-muted-foreground">
+                Package Type:
+              </p>
+              <p className="text-sm text-muted-foreground">Furniture</p>
             </div>
           </div>
         </Card>

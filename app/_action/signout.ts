5aaa0dcd2248fafdc30e9c478db99d3cfd021aaa
matellordson <snr.mn.lordson@gmail.com>
2024-09-14 13:25:38 +0100

    cmt

--- app/_action/signout.ts
+++ app/_action/signout.ts
@@ -1,28 +1,28 @@
-import { lucia, sessionId } from "@/utils/auth";
-import { cookies } from "next/headers";
+// import { lucia } from "@/utils/auth";
+// import { cookies } from "next/headers";
 
-export default async function Signout() {
-  try {
-    const session = await sessionId;
+// export default async function Signout() {
+//   try {
+//     const session = await sessionId;
 
-    if (!session) {
-      return {
-        error: "unauthorized",
-      };
-    }
+//     if (!session) {
+//       return {
+//         error: "unauthorized",
+//       };
+//     }
 
-    await lucia.invalidateSession(session.name);
+//     await lucia.invalidateSession(session.name);
 
-    const sessionCookie = lucia.createBlankSessionCookie();
+//     const sessionCookie = lucia.createBlankSessionCookie();
 
-    cookies().set(
-      sessionCookie.name,
-      sessionCookie.value,
-      sessionCookie.attributes,
-    );
-  } catch (error: any) {
-    return {
-      error: error.message,
-    };
-  }
-}
+//     cookies().set(
+//       sessionCookie.name,
+//       sessionCookie.value,
+//       sessionCookie.attributes,
+//     );
+//   } catch (error: any) {
+//     return {
+//       error: error.message,
+//     };
+//   }
+// }

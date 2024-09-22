831d6db7ed6ddd5d366ab06af79b0b33d1663682
matellordson <snr.mn.lordson@gmail.com>
2024-09-22 09:51:22 +0100

    cmt

--- app/admin-support/page.tsx
+++ app/admin-support/page.tsx
@@ -9,46 +9,6 @@ import { Search, Plus, ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import { supabase } from "@/src/db/supabase";
 
-interface Contact {
-  id: string;
-  name: string;
-  avatar: string;
-  status: string;
-}
-
-const initialContacts: Contact[] = [
-  {
-    id: "1",
-    name: "Alice Johnson",
-    avatar: "/placeholder.svg?height=40&width=40",
-    status: "Online",
-  },
-  {
-    id: "2",
-    name: "Bob Smith",
-    avatar: "/placeholder.svg?height=40&width=40",
-    status: "In a meeting",
-  },
-  {
-    id: "3",
-    name: "Charlie Brown",
-    avatar: "/placeholder.svg?height=40&width=40",
-    status: "Offline",
-  },
-  {
-    id: "4",
-    name: "Diana Prince",
-    avatar: "/placeholder.svg?height=40&width=40",
-    status: "Away",
-  },
-  {
-    id: "5",
-    name: "Ethan Hunt",
-    avatar: "/placeholder.svg?height=40&width=40",
-    status: "Do not disturb",
-  },
-];
-
 export default async function AdminSuppoortPAge() {
   // const [searchTerm, setSearchTerm] = useState("");
 
@@ -72,11 +32,8 @@ export default async function AdminSuppoortPAge() {
       <ScrollArea className="flex-grow">
         <div className="space-y-4 p-4">
           {data?.map((customer) => (
-            <Link href={`/admin-support/${customer.id}`}>
-              <div
-                key={customer.id}
-                className="flex items-center space-x-4 rounded-lg p-2 hover:bg-accent hover:text-foreground"
-              >
+            <Link href={`/admin-support/${customer.id}`} key={customer.id}>
+              <div className="flex items-center space-x-4 rounded-lg p-2 hover:bg-accent hover:text-foreground">
                 <Avatar>
                   <AvatarFallback className="bg-gradient-to-r from-neutral-200 to-neutral-300 text-foreground dark:from-neutral-500 dark:to-neutral-700">
                     {customer.user_name

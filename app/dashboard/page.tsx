a8eb8b5f054f6245c1f3806439053f72b577473a
matellordson <snr.mn.lordson@gmail.com>
2024-09-28 00:11:53 +0100

    cmt

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,14 @@ import { validateRequest } from "@/utils/auth";
 import AdminDashboard from "../admin-dashboard/page";
 import CustomerDashboardPage from "../_customer-dashboard/page";
 import { Support } from "@/components/support";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
   const currentUser = await validateRequest();
+
+  if (!currentUser) {
+    redirect("/auth/signin");
+  }
 
   if (currentUser?.user?.role === "admin") {
     return <AdminDashboard />;

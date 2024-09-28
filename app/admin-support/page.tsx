9005b1a529e3f6e36880009ba901254a652185ec
matellordson <snr.mn.lordson@gmail.com>
2024-09-28 23:02:30 +0100

    cmt

--- app/admin-support/page.tsx
+++ app/admin-support/page.tsx
@@ -1,11 +1,21 @@
 import { cookies } from "next/headers";
 import { supabase } from "@/src/db/supabase";
 import AdminSupportPage from "./admin-support-page";
+import { validateRequest } from "@/utils/auth";
+import { notFound, redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
   const { data, error } = await supabase.from("user").select("id, user_name");
+  const { user } = await validateRequest();
+
+  if (!user) {
+    redirect("/auth/signin");
+  }
+  if (user.role !== "admin") {
+    notFound();
+  }
 
   if (error) {
     console.error("Error fetching users:", error);
--- components/admin-support.tsx
+++ components/admin-support.tsx
@@ -1,75 +1,20 @@
-// "use client";
-
-// import * as React from "react";
-// import { MessageSquareText } from "lucide-react";
-// import { Button } from "@/components/ui/button";
-// import Link from "next/link";
-// import { supabase } from "@/src/db/supabase";
-
-// export function AdminSupport() {
-//   const [hasNotification, setHasNotification] = React.useState(false);
-
-//   React.useEffect(() => {
-//     const channel = supabase
-//       .channel("admin_notification")
-//       .on(
-//         "postgres_changes",
-//         {
-//           event: "INSERT",
-//           schema: "public",
-//           table: "chat",
-//         },
-//         (payload) => {
-//           console.log("Change received!", payload);
-//           setHasNotification(true);
-//         },
-//       )
-//       .subscribe();
-
-//     return () => {
-//       supabase.removeChannel(channel);
-//     };
-//   }, []);
-
-//   return (
-//     <div className="fixed bottom-4 right-4 z-50">
-//       <Button
-//         asChild
-//         variant="outline"
-//         size="icon"
-//         className="h-12 w-12 rounded-full bg-background/80 backdrop-blur-sm transition-all hover:bg-background/90 hover:shadow-lg"
-//       >
-//         <Link href={`admin-support`}>
-//           <span className="sr-only">Contact Support</span>
-//           <MessageSquareText className="h-6 w-6" />
-//           {hasNotification && (
-//             <span className="absolute -right-0.5 -top-0.5 h-3 w-3 rounded-full bg-destructive" />
-//           )}
-//         </Link>
-//       </Button>
-//     </div>
-//   );
-// }
-
 "use client";
 
 import * as React from "react";
 import { MessageSquareText } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { supabase } from "@/src/db/supabase";
 
 export default function AdminSupport() {
   const [hasNotification, setHasNotification] = React.useState(false);
-  const pathname = usePathname();
+  const router = useRouter();
 
   React.useEffect(() => {
     // Check localStorage on component mount
     const storedNotification = localStorage.getItem("adminNotification");
-    if (storedNotification === "true") {
-      setHasNotification(true);
-    }
+    setHasNotification(storedNotification === "true");
 
     const channel = supabase
       .channel("admin_notification")
@@ -93,13 +38,12 @@ export default function AdminSupport() {
     };
   }, []);
 
-  React.useEffect(() => {
-    // Check if the current path is the admin-support page
-    if (pathname === "/admin-support") {
-      setHasNotification(false);
-      localStorage.removeItem("adminNotification");
-    }
-  }, [pathname]);
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    setHasNotification(false);
+    localStorage.removeItem("adminNotification");
+    router.push("/admin-support");
+  };
 
   return (
     <div className="fixed bottom-4 right-4 z-50">
@@ -109,7 +53,7 @@ export default function AdminSupport() {
         size="icon"
         className="h-12 w-12 rounded-full bg-background/80 backdrop-blur-sm transition-all hover:bg-background/90 hover:shadow-lg"
       >
-        <Link href="/admin-support">
+        <Link href="/admin-support" onClick={handleClick}>
           <span className="sr-only">Contact Support</span>
           <MessageSquareText className="h-6 w-6" />
           {hasNotification && (

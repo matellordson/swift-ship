451d95eeba35a2c4406adf5bb5bee03e53ee82cb
matellordson <snr.mn.lordson@gmail.com>
2024-10-01 18:11:29 +0100

    cmt

--- components/landing/products.tsx
+++ components/landing/products.tsx
@@ -10,7 +10,7 @@ export function Products() {
             <p className="scroll-m-20 px-3 pb-1 text-2xl font-semibold tracking-tight text-secondary-foreground first:mt-0 dark:text-secondary lg:px-6 lg:text-4xl">
               We ship anything
             </p>
-            <p className="mb-3 px-3 leading-6 text-secondary-foreground dark:text-secondary lg:mt-2 lg:px-6 lg:pr-20 lg:text-lg [&:not(:first-child)]:mt-1">
+            <p className="mb-3 px-3 leading-6 text-slate-700 lg:mt-2 lg:px-6 lg:pr-20 lg:text-lg [&:not(:first-child)]:mt-1">
               No package is too big or small! Whether it's a heartfelt gift or a
               vintage motorcycle, our experts ensure secure packing and prompt
               delivery. With flexible options and exceptional customer service,

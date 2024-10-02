88912870b010b66d7b2d26c020179142458b7bcc
matellordson <snr.mn.lordson@gmail.com>
2024-10-02 01:54:00 +0100

    cmt

--- components/landing/freight.tsx
+++ components/landing/freight.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
-import truck from "@/app/public/truck.jpg";
+import cargo from "@/app/public/cargo.jpg";
 
 export default function Freight() {
   return (
-    <div className="items-start justify-between lg:flex">
+    <div className="items-start justify-between lg:m-5 lg:flex">
       <div className="pt-4 lg:pt-10">
-        <p className="scroll-m-20 px-3 pb-1 text-2xl font-semibold tracking-tight first:mt-0 lg:px-6 lg:text-4xl">
+        <p className="scroll-m-20 px-3 pb-1 text-2xl font-semibold tracking-tight first:mt-0 lg:px-10 lg:pr-24 lg:text-4xl lg:marker:pl-20">
           Redefined logistics, ensuring timely delivery whether by Land, Air, or
           Sea.
         </p>
-        <p className="mb-3 px-3 leading-6 text-muted-foreground lg:mt-2 lg:px-6 lg:pr-20 lg:text-lg [&:not(:first-child)]:mt-1">
+        <p className="mb-3 px-3 leading-6 text-muted-foreground lg:mt-2 lg:px-10 lg:text-lg [&:not(:first-child)]:mt-1">
           At Swift Ship, timely delivery is our priority. Whether by land, air,
           or sea, we use advanced logistics and trusted partnerships to ensure
           your orders arrive on schedule. Your satisfaction is our commitment!.
@@ -17,9 +17,9 @@ export default function Freight() {
       </div>
       <div className="h-fit rounded-lg px-3 lg:rounded-none lg:px-0">
         <Image
-          src={truck}
+          src={cargo}
           alt="truck"
-          className="h-[330px] w-[1800px] rounded-lg object-cover dark:opacity-70 lg:h-[310px] lg:rounded-none"
+          className="h-[200px] w-[1800px] rounded-lg object-cover dark:opacity-90 lg:h-[310px]"
         />
       </div>
     </div>

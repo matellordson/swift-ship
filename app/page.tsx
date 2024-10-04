274f1689fb8c871b06a4a942086222e219026f47
matellordson <snr.mn.lordson@gmail.com>
2024-10-04 17:52:30 +0100

    cmt

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,8 @@
-import { redirect } from "next/navigation";
-import { validateRequest } from "@/utils/auth";
-import { Introduction } from "@/components/landing/introduction";
-import Freight from "@/components/landing/freight";
-import { Shops } from "@/components/landing/shops";
-import { Products } from "@/components/landing/products";
 import HomePage from "@/components/homepage";
 
 export default async function Home() {
-  // const user = await validateRequest();
-  // if (!user) {
-  //   redirect("/auth/signin");
-  // }
   return (
     <div className="mx-auto max-w-[200rem]">
-      {/* <Introduction />
-      <Freight />
-      <Products /> */}
       <HomePage />
     </div>
   );
--- components/nav-items.tsx
+++
@@ -1,8 +0,0 @@
-import { CircleUserRound } from "lucide-react";
-import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
-import { DropdownMenuContent, DropdownMenuTrigger } from "./ui/dropdown-menu";
-
-export async function NavbarItems() {
-  return <div></div>;
-}

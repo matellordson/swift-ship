2ccb019a31fd7cac51374ee38b0d348b1d10958a
matellorson <snr.mn.lordson@gmail.com>
2024-11-01 15:44:27 +0100

    cmt

--- app/(links)/track-shipment/page.tsx
+++ app/(links)/track-shipment/page.tsx
@@ -1,8 +1,12 @@
-import Logo from "@/components/logo";
+"use client";
+
 import PackageTimeline from "@/components/PackageTimeline";
-import Link from "next/link";
+import Image from "next/image";
+import swiftShipLogo from "@/app/public/logo.svg";
+import { useRouter } from "next/navigation";
 
 export default function TrackShipment() {
+  const router = useRouter();
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <nav className="mb-7 border-b bg-background">
@@ -10,7 +14,22 @@ export default function TrackShipment() {
           <div className="flex h-16 items-center justify-between">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <Logo />
+                <div
+                  className="flex items-center space-x-2"
+                  onClick={router.back}
+                >
+                  <Image
+                    src={swiftShipLogo}
+                    height={50}
+                    width={50}
+                    priority
+                    alt="Swift Ship logo"
+                    className="m-0 h-24 p-0"
+                  />
+                  <p className="text-lg font-semibold tracking-tight text-primary lg:text-base">
+                    Swift Ship
+                  </p>
+                </div>
               </div>
             </div>
           </div>
